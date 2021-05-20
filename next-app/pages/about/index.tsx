eed8d2361a25ccda0a8b9fb8931fc4ad8a050c9c
joey desjardins <jwdesjardin@gmail.com>
2021-05-20 13:49:07 -0700

    add markup and styles to about page to match mobile design files

--- next-app/pages/about/index.tsx
+++ next-app/pages/about/index.tsx
@@ -9,6 +9,8 @@ import { HowCard } from '../../components/HowCard'
 import { Footer } from '../../components/Footer'
 
 import styles from './index.module.css'
+import { HeroCard } from '../../components/HeroCard'
+import { HQCard } from '../../components/HQCard'
 
 const About = () => {
 	return (
@@ -24,71 +26,81 @@ const About = () => {
 				<Navbar></Navbar>
 
 				{/* HERO */}
-				<div className={styles.hero_container}>
-					<div className={styles.hero_content}>
-						<h1>Great coffee made simple.</h1>
+				<HeroCard
+					description='Coffeeroasters began its journey of exotic discovery in 1999, highlighting stories of 
+					coffee from around the world. We have since been dedicated to bring the perfect cup - from 
+					bean to brew - in every shipment.'
+					title='About us'
+					bgURL='/assets/about/mobile/image-hero-whitecup.jpg'
+				></HeroCard>
+
+				{/* COMMITMENT */}
+				<div className={styles.commitment_container}>
+					<div className={styles.commitment_imagebox}>
+						<img src='/assets/about/mobile/image-commitment.jpg' alt='' />
+					</div>
+
+					<div className={styles.commitment_textcontent}>
+						<h2>Our commitment</h2>
 						<p>
-							Start your mornings with the world’s best coffees. Try our expertly curated artisan
-							coffees from our best roasters delivered directly to your door, at your schedule.
+							We’re built on a simple mission and a commitment to doing good along the way. We want
+							to make it easy for you to discover and brew the world’s best coffee at home. It all
+							starts at the source. To locate the specific lots we want to purchase, we travel
+							nearly 60 days a year trying to understand the challenges and opportunities in each of
+							these places. We collaborate with exceptional coffee growers and empower a global
+							community of farmers through with well above fair-trade benchmarks. We also offer
+							training, support farm community initiatives, and invest in coffee plant science.
+							Curating only the finest blends, we roast each lot to highlight tasting profiles
+							distinctive to their native growing region.
 						</p>
-						<button className='button-primary'>Create your plan</button>
 					</div>
 				</div>
 
-				{/* WHY Section */}
-				<div className={styles.why_container}>
-					<div className={styles.why_textcontent}>
-						<h2>Why choose us?</h2>
+				{/* QUALITY */}
+				<div className={styles.quality_container}>
+					<div className={styles.quality_imagebox}>
+						<img src='/assets/about/mobile/image-quality.jpg' alt='' />
+					</div>
+
+					<div className={styles.quality_textcontent}>
+						<h2>Uncompromising quality</h2>
 						<p>
-							A large part of our role is choosing which particular coffees will be featured in our
-							range. This means working closely with the best coffee growers to give you a more
-							impactful experience on every level.
+							Although we work with growers who pay close attention to all stages of harvest and
+							processing, we employ, on our end, a rigorous quality control program to avoid
+							over-roasting or baking the coffee dry. Every bag of coffee is tagged with a roast
+							date and batch number. Our goal is to roast consistent, user-friendly coffee, so that
+							brewing is easy and enjoyable.
 						</p>
 					</div>
-					<WhyCard
-						imageURL='/assets/home/desktop/icon-coffee-bean.svg'
-						title='Best quality'
-						description='Discover an endless variety of the world’s best artisan coffee from each of our roasters.'
-						altText='coffe bean icon'
-					></WhyCard>
-					<WhyCard
-						imageURL='/assets/home/desktop/icon-gift.svg'
-						title='Exclusive benefits'
-						description='Special offers and swag when you subscribe, including 30% off your first shipment.'
-						altText='gift icon'
-					></WhyCard>
-					<WhyCard
-						imageURL='/assets/home/desktop/icon-truck.svg'
-						title='Free shipping'
-						description='We cover the cost and coffee is delivered fast. Peak freshness: guaranteed.'
-						altText='truck icon'
-					></WhyCard>
 				</div>
 
-				{/* HOW Section */}
-				<div className={styles.how_container}>
-					<h2>How it works</h2>
-
-					<HowCard
-						title='Pick your coffee'
-						description='Select from our evolving range of artisan coffees. Our beans are ethically
-						sourced and we pay fair prices for them. There are new coffees in all profiles
-						every month for you to try out.'
-						number='01'
-					/>
-					<HowCard
-						title='Choose the frequency'
-						description='Customize your order frequency, quantity, even your roast style and grind type.
-						Pause, skip or cancel your subscription with no commitment through our online portal.'
-						number='02'
-					/>
-					<HowCard
-						title='Receive and enjoy!'
-						description='We ship your package within 48 hours, freshly roasted. Sit back and enjoy award-winning
-						world-class coffees curated to provide a distinct tasting experience.'
-						number='03'
-					/>
-					<button className='button-primary'>Create Your Plan</button>
+				{/* HQ */}
+				<div className={styles.hq_container}>
+					<h2 className='alternate-title'>Our headquarters</h2>
+					<HQCard
+						imageURL='/assets/about/desktop/illustration-uk.svg'
+						country='United Kingdom'
+						street='68 Asfordby Rd'
+						city='Alcaston'
+						zipcode='gSY6 1YA'
+						phone='+44 1241 918425'
+					></HQCard>
+					<HQCard
+						imageURL='/assets/about/desktop/illustration-canada.svg'
+						country='Canada'
+						street='1528 Eglinton Avenue'
+						city='Toronto'
+						zipcode='Ontario M4P 1A6'
+						phone='+1 416 485 2997'
+					></HQCard>
+					<HQCard
+						imageURL='/assets/about/desktop/illustration-australia.svg'
+						country='Australia'
+						street='36 Swanston Street'
+						city='Kewell'
+						zipcode='Victoria'
+						phone='+61 4 9928 3629'
+					></HQCard>
 				</div>
 
 				<Footer></Footer>
