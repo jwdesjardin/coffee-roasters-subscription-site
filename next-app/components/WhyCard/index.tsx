9c248b41d2d793c02e11088393708a4e4bed4aa5
joey desjardins <jwdesjardin@gmail.com>
2021-05-20 11:56:55 -0700

    adds WhyCard component

---
+++ next-app/components/WhyCard/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from './index.module.css'
+
+interface WhyCardProps {
+	imageURL: string
+	title: string
+	description: string
+	altText: string
+}
+
+export const WhyCard: React.FC<WhyCardProps> = ({ imageURL, title, description, altText }) => {
+	return (
+		<div className={styles.why_card_container}>
+			<img src={imageURL} alt={altText} width='72px' />
+			<div className={styles.why_card_textcontent}>
+				<h3>{title}</h3>
+				<p>{description}</p>
+			</div>
+		</div>
+	)
+}
