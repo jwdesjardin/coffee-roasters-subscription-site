378d60e9bcbbe4f6c3aac6e76576e9c44d6afcae
joey desjardins <jwdesjardin@gmail.com>
2021-05-20 21:16:49 -0700

    add markups and styles for the base Options component and the dependant Option component

---
+++ next-app/components/Options/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { option } from '../../types'
+import { ArrowIcon } from '../Icons'
+import styles from './index.module.css'
+
+// OPTIONs COMPONENT
+
+interface OptionsProps {
+	question: string
+	options: option[]
+}
+
+export const Options: React.FC<OptionsProps> = ({ question, options }) => {
+	return (
+		<div className={styles.container}>
+			<div className={styles.question_container}>
+				<h4>{question}</h4>
+				<button>
+					<ArrowIcon></ArrowIcon>
+				</button>
+			</div>
+			<div className={styles.options_container}>
+				{options.map((option) => (
+					<Option option={option}></Option>
+				))}
+			</div>
+		</div>
+	)
+}
+
+// OPTION COMPONENT
+
+interface OptionProps {
+	option: option
+}
+
+const Option: React.FC<OptionProps> = ({ option }) => {
+	return (
+		<div className={styles.option_container}>
+			<h4>{option.value}</h4>
+			<p>{option.description}</p>
+		</div>
+	)
+}
