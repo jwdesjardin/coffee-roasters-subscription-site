aadd8e3a7cb470093698b9cfb7d24e5c6f2b7a9e
joey desjardins <jwdesjardin@gmail.com>
2021-05-21 10:08:07 -0700

    add tablet media queries to Why card

--- next-app/components/WhyCard/index.tsx
+++ next-app/components/WhyCard/index.tsx
@@ -11,7 +11,9 @@ interface WhyCardProps {
 export const WhyCard: React.FC<WhyCardProps> = ({ imageURL, title, description, altText }) => {
 	return (
 		<div className={styles.why_card_container}>
-			<img src={imageURL} alt={altText} width='72px' />
+			<div className={styles.imagebox}>
+				<img src={imageURL} alt={altText} width='72px' />
+			</div>
 			<div className={styles.why_card_textcontent}>
 				<h3>{title}</h3>
 				<p>{description}</p>
