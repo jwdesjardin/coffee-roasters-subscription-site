4604e7aed9bd4c095ca9bc20ebb9b9d08b33399b
joey desjardins <jwdesjardin@gmail.com>
2021-05-21 14:10:14 -0700

    completes desktop media queries for WhatCard Component

--- next-app/components/WhatCard/index.tsx
+++ next-app/components/WhatCard/index.tsx
@@ -10,9 +10,9 @@ interface WhatCardProps {
 
 export const WhatCard: React.FC<WhatCardProps> = ({ imageURL, title, description, altText }) => {
 	return (
-		<div className={styles.what_card_container}>
+		<div className={styles.container}>
 			<img src={imageURL} alt={altText} width='200px' height='151px' />
-			<div className={styles.what_card_textcontent}>
+			<div className={styles.textcontent}>
 				<h3>{title}</h3>
 				<p>{description}</p>
 			</div>
