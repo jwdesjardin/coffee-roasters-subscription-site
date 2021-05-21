8ffbcec225fdd46b5e891bc8240585f36dd75688
joey desjardins <jwdesjardin@gmail.com>
2021-05-21 10:07:23 -0700

    add link functionality to footer

--- next-app/components/Footer/index.tsx
+++ next-app/components/Footer/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './index.module.css'
+import Link from 'next/link'
 
 export const Footer = () => {
 	return (
@@ -12,24 +13,18 @@ export const Footer = () => {
 				height='26px'
 			/>
 			<div className={styles.links_container}>
-				<a href='' className={styles.links}>
-					HOME
-				</a>
-				<a href='' className={styles.links}>
-					ABOUT US
-				</a>
-				<a href='' className={styles.links}>
-					CREATE YOUR PLAN
-				</a>
+				<Link href='/home'>HOME</Link>
+				<Link href='/about'>ABOUT US</Link>
+				<Link href='/subscribe'>CREATE YOUR PLAN</Link>
 			</div>
 			<div className={styles.social_container}>
-				<a href='' className={styles.links}>
+				<a href='#' className={styles.links}>
 					<img src='/assets/shared/desktop/icon-facebook.svg' alt='' width='24px' height='24px' />
 				</a>
-				<a href='' className={styles.links}>
+				<a href='#' className={styles.links}>
 					<img src='/assets/shared/desktop/icon-twitter.svg' alt='' width='24px' height='24px' />
 				</a>
-				<a href='' className={styles.links}>
+				<a href='#' className={styles.links}>
 					<img src='/assets/shared/desktop/icon-instagram.svg' alt='' width='24px' height='24px' />
 				</a>
 			</div>
