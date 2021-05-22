9bdb6ff16ef9a72a51271a135237fb1489e2d942
joey desjardins <jwdesjardin@gmail.com>
2021-05-22 15:31:12 -0700

    add media queries to heroCard

--- next-app/pages/subscribe/index.tsx
+++ next-app/pages/subscribe/index.tsx
@@ -42,7 +42,7 @@ const Subscribe = () => {
 					title='Create a plan'
 					description='Build a subscription plan that best fits your needs. We offer an assortment of the best 
           artisan coffees from around the globe delivered fresh to your door.'
-					imageClassName='white-cup-image'
+					imageClassName='black-cup-image'
 				></HeroCard>
 
 				{/* Instructions */}
