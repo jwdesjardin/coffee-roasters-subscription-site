1e3ecd97539c4d82069a0fdfadefc63b519f4c4c
joey desjardins <jwdesjardin@gmail.com>
2021-05-22 14:39:19 -0700

    fix react Svg typo

--- next-app/components/Icons/index.tsx
+++ next-app/components/Icons/index.tsx
@@ -4,7 +4,7 @@ export const ArrowIcon = () => {
 			<path
 				d='M15.949.586l2.828 2.828-9.096 9.096L.586 3.414 3.414.586l6.267 6.267z'
 				fill='#0E8784'
-				fill-rule='nonzero'
+				fillRule='nonzero'
 			/>
 		</svg>
 	)
