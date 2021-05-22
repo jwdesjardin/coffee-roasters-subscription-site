bd7f25264ab0adff95d69179185a098b49163c39
joey desjardins <jwdesjardin@gmail.com>
2021-05-22 11:37:20 -0700

    update about page

--- next-app/pages/about/index.tsx
+++ next-app/pages/about/index.tsx
@@ -3,9 +3,6 @@ import Image from 'next/image'
 import React from 'react'
 
 import { Navbar } from '../../components/Navbar'
-import { WhatCard } from '../../components/WhatCard'
-import { WhyCard } from '../../components/WhyCard'
-import { HowCard } from '../../components/HowCard'
 import { Footer } from '../../components/Footer'
 
 import styles from './index.module.css'
@@ -31,13 +28,14 @@ const About = () => {
 					coffee from around the world. We have since been dedicated to bring the perfect cup - from 
 					bean to brew - in every shipment.'
 					title='About us'
-					bgURL='/assets/about/mobile/image-hero-whitecup.jpg'
+					imageClassName='black-cup-image'
 				></HeroCard>
 
 				{/* COMMITMENT */}
-				<div className={styles.commitment_container}>
+				<section className={styles.commitment_container}>
 					<div className={styles.commitment_imagebox}>
-						<img src='/assets/about/mobile/image-commitment.jpg' alt='' />
+						{/* <img src='/assets/about/mobile/image-commitment.jpg' alt='' /> */}
+						<img src='/assets/about/desktop/image-commitment.jpg' alt='' />
 					</div>
 
 					<div className={styles.commitment_textcontent}>
@@ -54,12 +52,13 @@ const About = () => {
 							distinctive to their native growing region.
 						</p>
 					</div>
-				</div>
+				</section>
 
 				{/* QUALITY */}
-				<div className={styles.quality_container}>
+				<section className={styles.quality_container}>
 					<div className={styles.quality_imagebox}>
-						<img src='/assets/about/mobile/image-quality.jpg' alt='' />
+						{/* <img src='/assets/about/mobile/image-quality.jpg' alt='' /> */}
+						<img src='/assets/about/desktop/image-quality.jpg' alt='' />
 					</div>
 
 					<div className={styles.quality_textcontent}>
@@ -72,36 +71,38 @@ const About = () => {
 							brewing is easy and enjoyable.
 						</p>
 					</div>
-				</div>
+				</section>
 
 				{/* HQ */}
-				<div className={styles.hq_container}>
+				<section className={styles.hq_container}>
 					<h2 className='alternate-title'>Our headquarters</h2>
-					<HQCard
-						imageURL='/assets/about/desktop/illustration-uk.svg'
-						country='United Kingdom'
-						street='68 Asfordby Rd'
-						city='Alcaston'
-						zipcode='gSY6 1YA'
-						phone='+44 1241 918425'
-					></HQCard>
-					<HQCard
-						imageURL='/assets/about/desktop/illustration-canada.svg'
-						country='Canada'
-						street='1528 Eglinton Avenue'
-						city='Toronto'
-						zipcode='Ontario M4P 1A6'
-						phone='+1 416 485 2997'
-					></HQCard>
-					<HQCard
-						imageURL='/assets/about/desktop/illustration-australia.svg'
-						country='Australia'
-						street='36 Swanston Street'
-						city='Kewell'
-						zipcode='Victoria'
-						phone='+61 4 9928 3629'
-					></HQCard>
-				</div>
+					<div className={styles.hq_card_container}>
+						<HQCard
+							imageURL='/assets/about/desktop/illustration-uk.svg'
+							country='United Kingdom'
+							street='68 Asfordby Rd'
+							city='Alcaston'
+							zipcode='gSY6 1YA'
+							phone='+44 1241 918425'
+						></HQCard>
+						<HQCard
+							imageURL='/assets/about/desktop/illustration-canada.svg'
+							country='Canada'
+							street='1528 Eglinton Avenue'
+							city='Toronto'
+							zipcode='Ontario M4P 1A6'
+							phone='+1 416 485 2997'
+						></HQCard>
+						<HQCard
+							imageURL='/assets/about/desktop/illustration-australia.svg'
+							country='Australia'
+							street='36 Swanston Street'
+							city='Kewell'
+							zipcode='Victoria'
+							phone='+61 4 9928 3629'
+						></HQCard>
+					</div>
+				</section>
 
 				<Footer></Footer>
 			</main>
